3ba7f2ad7011a4b521ce1986538b1a60fbfaaa0c
amolmm9 <amolmm9@gmail.com>
2022-02-04 16:07:14 +0530

    Update Program.cs

--- myfirstapi/Program.cs
+++ myfirstapi/Program.cs
@@ -13,7 +13,7 @@ namespace myfirstapi
     {
         public static void Main(string[] args)
         {
-            # Test 123
+            
             CreateHostBuilder(args).Build().Run();
         }
 
